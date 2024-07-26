fe64d756ea23b3dbca7addea0f4ac1c70eaf6d79
Yogesh <139439956+yogesh7833@users.noreply.github.com>
2024-07-26 12:32:38 +0530

    Update DecodeString.cpp

--- Stack/DecodeString.cpp
+++ Stack/DecodeString.cpp
@@ -1,1 +1,45 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string>st;
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if(ch==']'){
+               string stringRepeat="";
+               while(!st.empty() && !isdigit(st.top()[0])){
+                string top=st.top();
+                stringRepeat+=top=="[" ? "":top;
+                st.pop();
+               }
+               string numericString="";
+               while(!st.empty() && isdigit(st.top()[0])){
+                     numericString+=st.top();
+                     st.pop();
+               }
+               reverse(numericString.begin(),numericString.end());
+               int n=stoi(numericString);
 
+               //finl deconding 
+               string currDecode="";
+               while(n--){
+                currDecode+=stringRepeat;
+               }
+               st.push(currDecode);
+            }
+            else{
+                string temp(1,ch);
+                st.push(temp);
+            }
+        }
+
+        string ans;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
